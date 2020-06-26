0db807abfe68c8f0d882d1d5dac4c084b88cd302
Marko Korevec <marko@korevec.net>
2020-06-26 12:29:27 +0200

    Update i.sh

--- i.sh
+++ i.sh
@@ -3,7 +3,6 @@
 echo "Downloading Alis..."
 loadkeys us
 curl https://raw.githubusercontent.com/mkorevec/upgraded-happiness/master/local.conf -o local.conf
-curl https://raw.githubusercontent.com/picodotdev/alis/master/download.sh -o download.sh
-./download.sh
+curl https://raw.githubusercontent.com/picodotdev/alis/master/download.sh | bash
 rm alis.conf
 mv local.conf alis.conf
