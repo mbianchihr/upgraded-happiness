93230f2c20eec33b660041a486369a9d7399712b
Marko Korevec <marko@korevec.net>
2020-06-26 12:22:35 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 echo "Downloading Alis..."
 loadkeys us
 curl https://raw.githubusercontent.com/picodotdev/alis/master/download.sh | bash
+curl https://raw.githubusercontent.com/mkorevec/upgraded-happiness/master/local.conf -o local.conf
 rm alis.conf
 mv local.conf alis.conf
-vim alis.conf
+vi alis.conf
